a9bedeb7792c1f4b44f4a32e9e31215659fec2ab
Michal <mike3129@gmail.com>
2020-06-09 21:13:58 +0200

    Plik main

---
+++ src/main/java/Kolekcje/Main.java
@@ -0,0 +1,10 @@
+package Kolekcje;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+        System.out.println("Witaj świecie 2");
+        System.out.println("hhehe");
+    }
+}
+
