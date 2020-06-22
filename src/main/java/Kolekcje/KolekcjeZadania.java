2f29faeb65d12229fe43610ad4cbdf9bbcd30d47
Michal <mike3129@gmail.com>
2020-06-22 21:14:11 +0200

    zadanie 3

--- src/main/java/Kolekcje/KolekcjeZadania.java
+++ src/main/java/Kolekcje/KolekcjeZadania.java
@@ -1,8 +1,8 @@
 package Kolekcje;
 
-import java.util.ArrayList;
-import java.util.Random;
-import java.util.Scanner;
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.*;
 
 public class KolekcjeZadania {
 
@@ -15,7 +15,7 @@ Stwórz listę Integerów. Wykonaj zadania:
 Sprawdź działanie aplikacji.
          */
 
-    public static void zad_1() {
+    public static void zad1() {
         Scanner scanner = new Scanner(System.in);
         ArrayList<Integer> liczby = new ArrayList<>();
         int i;
@@ -34,7 +34,79 @@ Sprawdź działanie aplikacji.
         System.out.println(liczby);
     }
 
+    /*
+ Stwórz oddzielnego maina, a w nim kolejną listę integerów. Wykonaj zadania:
+- dodaj do listy 10 liczb losowych
+- oblicz sumę elementów na liście (wypisz ją)
+- oblicz średnią elementów na liście (wypisz ją)
+- stwórz kopię listy i ją posortuj: (Collections.sort( listaDoPosortowania) - sortuje listę)
+(przykład użycia Collections.sort(lista):
+ArrayList<Integer> liczby = new ArrayList<>();
+liczby.add(5);
+liczby.add(10);
+liczby.add(15);
+Collections.sort(liczby);
+- podaj medianę elementów na liście posortowanej (wypisz ją)
+- znajdź największy i najmniejszy element na liście która nie była sortowana. Zrób to pętlą for
+- po znalezieniu elementu znajdź index elementu maksymalnego używając kolejnej pętli for
+- po znalezieniu elementów (największy i najmniejszy) znajdź index posługując się metodą indexof
+Sprawdź działanie aplikacji.Stwórz oddzielnego maina, a w nim kolejną listę integerów.
 
+     */
+
+    public static void zad2() {
+        Random generator = new Random();
+        ArrayList<Integer> lista = new ArrayList<>();
+        int i;
+        //wypisanie losowych liczb
+        for (i = 0; i < 10; i++)
+            lista.add(generator.nextInt(30));
+        System.out.println(lista);
+
+        //sumowanie losowych liczb
+        int suma = 0;
+        for (i = 0; i < lista.size(); i++) {
+            suma = lista.get(i) + suma;
+        }
+
+        System.out.println("suma listy: " + suma);
+        System.out.println("średnia liczb wynosi: " + suma / lista.size());
+
+        //sortowanie listy
+        ArrayList<Integer> listaPosortowana = lista;
+        Collections.sort(listaPosortowana);
+        System.out.println(listaPosortowana);
+
+        int a = listaPosortowana.get(4);
+        int b = listaPosortowana.get(5);
+        float mediana = (a + b) / 2;
+
+        System.out.println("Mediana listy wynosi " + mediana);
+
+        // szukanie najmniejszego elemntu w nieposortowanej liście
+        int min = 0;
+        for (i = 1; i < lista.size(); i++) {
+            if (lista.get(0) < lista.get(i)) {
+                min = lista.get(0);
+            } else {
+                min = lista.get(i);
+            }
+        }
+        System.out.println("najmniejszy element to: " + min);
+        System.out.println("index elementu to: " + lista.indexOf(min));
+
+        //szukanie największego elemntu
+        int max = 0;
+        for (i = 1; i < lista.size(); i++) {
+            if (lista.get(0) > lista.get(i)) {
+                max = lista.get(0);
+            } else {
+                max = lista.get(i);
+            }
+        }
+        System.out.println("największy element to: " + max);
+        System.out.println("index elementu to: "+ lista.indexOf(max));
+    }
 
 
 }
--- src/main/java/Kolekcje/Main.java
+++ src/main/java/Kolekcje/Main.java
@@ -4,8 +4,8 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args){
-        System.out.println("Wyświetlmy listę");
-        KolekcjeZadania.zad_1();
+
+        KolekcjeZadania.zad2();
 
     }
 }
