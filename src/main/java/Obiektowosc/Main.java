813f410a66df3f6d6cc1b417961232410b3b276e
Michal <mike3129@gmail.com>
2020-06-25 21:47:34 +0200

    uaktualnienie maina

--- src/main/java/Obiektowosc/Main.java
+++ src/main/java/Obiektowosc/Main.java
@@ -6,6 +6,8 @@ public class Main {
 //        Produkt.podajCeneBrutto(256.55);
         Rachunek rachunek = new Rachunek();
         rachunek.dodajProdukt(new Produkt("Masło",2.55,PodatekProduktu.VAT23));
+        rachunek.dodajProdukt(new Produkt("Chleb",1.55,PodatekProduktu.VAT23));
+        rachunek.dodajProdukt(new Produkt("Ser",6.77,PodatekProduktu.VAT23));
         rachunek.wyswietlRachunek();
     }
 }
